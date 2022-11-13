451807bfc24eee456c34e29f7b2485a151522f99
Ireanne-Omega <116430505+Ireanne-Omega@users.noreply.github.com>
2022-11-13 12:49:28 +0800

    This is fixed.

---
+++ README.md
@@ -0,0 +1,4 @@
+#ContactTracing_Dictionary.py
+A Program to Collect Information on Contacts
+Programmed by: Alliah Bulalacao & Ireanne Omega
+Version Created: 02-28-2022
---
+++ main.py
@@ -0,0 +1,20 @@
+"""
+addressbook.py
+A Program to Collect Information on Contacts
+Programmed by: Ireanne Omega
+Version Created: 01-13-2022
+
+"""
+# ......................................All rights received.................................................
+
+print(".....................................")
+print("\nCONTACT TRACING ")
+print("PROGRAMMED BY:")
+print("Ireanne Omega")
+print("Version Created: 02-28-2022")
+print(".....................................")
+
+# ...L I S T & V A R I A B L E S...........................................................................
+
+contacts = [ ]
+addressBook = []
